b2fbd902f15fb11c8d71b906dbf4b16835604ad9
@gdmec07150744 <1216847952@qq.com>
2016-09-22 11:19:31 +0800

    生命周期

---
+++ app/src/main/java/com/example/min123/work2/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.min123.work2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";//logt
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called with: " + "start");//logm
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,  "resume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,  "stop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,  "pause");
+    }
+
+}
